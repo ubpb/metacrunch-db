b08d806e93cb0d580f7f87408305ad63761a0c46
René Sprotte <renspr@gmail.com>
2017-10-16 13:41:04 +0200

    Add more tests.

--- spec/metacrunch/db/destination_spec.rb
+++ spec/metacrunch/db/destination_spec.rb
@@ -41,6 +41,65 @@ describe Metacrunch::DB::Destination do
     end
   end
 
+  context "when data exists" do
+    before {
+      dest = Metacrunch::DB::Destination.new(DB[:users])
+      10.times { |i| dest.write({id: i, name: "name-#{i}"}) }
+    }
+
+    describe "#write" do
+      context "when use_upsert = true" do
+        subject { Metacrunch::DB::Destination.new(DB[:users], use_upsert: true) }
+
+        it "updates existing data" do
+          data = []
+
+          10.times do |i|
+            data << {id: i, name: "updated-name-#{i}"}
+          end
+
+          subject.write(data)
+
+          expect(DB[:users].where(id: 2).first).to eq({:id=>2, :name=>"updated-name-2"})
+        end
+      end
+
+      context "when use_upsert = false" do
+        subject { Metacrunch::DB::Destination.new(DB[:users], use_upsert: false) }
+
+        it "raises an error" do
+          data = []
+
+          10.times do |i|
+            data << {id: i, name: "updated-name-#{i}"}
+          end
+
+          expect {
+            subject.write(data)
+          }.to raise_error(Sequel::UniqueConstraintViolation)
+        end
+      end
+    end
+  end
+
+  context "when use_upsert = true and no data exists" do
+    subject { Metacrunch::DB::Destination.new(DB[:users], use_upsert: true) }
+
+    describe "#write" do
+      it "writes data into database" do
+        data = []
+
+        10.times do |i|
+          data << {id: i, name: "name-#{i}"}
+        end
+
+        subject.write(data)
+
+        expect(DB[:users].count).to eq(10)
+      end
+    end
+  end
+
   describe "#close" do
     subject { Metacrunch::DB::Destination.new(DB[:users]) }
 
